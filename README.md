5c1a70cd76346274a682a195cb45f06fd79777ca
Sinan Özmen <46645793+SaveEnergy@users.noreply.github.com>
2025-04-25 17:13:16 +0200

    Implementation of caddy-dns/autodns

---
+++ README.md
@@ -0,0 +1,64 @@
+Autodns module for Caddy
+===========================
+
+This package contains a DNS provider module for [Caddy](https://github.com/caddyserver/caddy). It can be used to manage DNS records with autodns.
+
+## Caddy module name
+
+```
+dns.providers.autodns
+```
+
+## Config examples
+
+To use this module for the ACME DNS challenge, [configure the ACME issuer in your Caddy JSON](https://caddyserver.com/docs/json/apps/tls/automation/policies/issuer/acme/) like so:
+
+```json
+{
+	"module": "acme",
+	"challenges": {
+		"dns": {
+			"provider": {
+				"name": "autodns",
+				"username": "{env.AUTODNS_USERNAME}",
+				"password": "{env.AUTODNS_PASSWORD}",
+				"endpoint": "{env.AUTODNS_ENDPOINT}",
+				"context": "{env.AUTODNS_CONTEXT}"
+			}
+		}
+	}
+}
+```
+
+### Caddyfile Configuration
+
+Caddyfile configuration
+
+#### Global Configuration
+
+```
+{
+  acme_dns autodns {
+    username "<username>"
+    password "<password>"
+  }
+}
+```
+
+#### Per-Site Configuration
+
+```
+tls {
+	dns autodns {
+		username {env.AUTODNS_USERNAME}
+		password {env.AUTODNS_PASSWORD}
+		endpoint {env.AUTODNS_ENDPOINT}
+		context {env.AUTODNS_CONTEXT}
+	}
+}
+```
+
+
+## Authenticating
+
+See [the associated README in the libdns package](https://github.com/libdns/autodns) for important information about credentials.
---
+++ autodns.go
@@ -0,0 +1,112 @@
+package autodns
+
+import (
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/libdns/autodns"
+)
+
+// Provider lets Caddy read and manipulate DNS records hosted by this DNS provider.
+type Provider struct{ *autodns.Provider }
+
+func init() {
+	caddy.RegisterModule(Provider{})
+}
+
+// CaddyModule returns the Caddy module information.
+func (Provider) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "dns.providers.autodns",
+		New: func() caddy.Module { return &Provider{new(autodns.Provider)} },
+	}
+}
+
+// Provision sets up the module. Implements caddy.Provisioner.
+func (p *Provider) Provision(ctx caddy.Context) error {
+	p.Provider.Username = caddy.NewReplacer().ReplaceAll(p.Provider.Username, "")
+	p.Provider.Password = caddy.NewReplacer().ReplaceAll(p.Provider.Password, "")
+	p.Provider.Endpoint = caddy.NewReplacer().ReplaceAll(p.Provider.Endpoint, "")
+	p.Provider.Context = caddy.NewReplacer().ReplaceAll(p.Provider.Context, "")
+	return nil
+}
+
+// UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
+//
+//	autodns {
+//	    secret_key string
+//	    organization_id string
+//	}
+//
+// **THIS IS JUST AN EXAMPLE AND NEEDS TO BE CUSTOMIZED.**
+func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "username":
+				if p.Provider.Username != "" {
+					return d.Err("Username already set")
+				}
+				if d.NextArg() {
+					p.Provider.Username = d.Val()
+				}
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			case "password":
+				if p.Provider.Password != "" {
+					return d.Err("Organization ID already set")
+				}
+				if d.NextArg() {
+					p.Provider.Password = d.Val()
+				}
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			default:
+				return d.Errf("unrecognized subdirective '%s'", d.Val())
+			}
+			case "Endpoint":
+				if p.Provider.Endpoint != "" {
+					return d.Err("Endpoint already set")
+				}
+				if d.NextArg() {
+					p.Provider.Endpoint = d.Val()
+				}
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			case "context":
+				if p.Provider.Context != "" {
+					return d.Err("Context already set")
+				}
+				if d.NextArg() {
+					p.Provider.Context = d.Val()
+				}
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+		}
+	}
+	if p.Provider.Username == "" {
+		return d.Err("missing Secret key")
+	}
+	if p.Provider.Password == "" {
+		return d.Err("missing Organization ID")
+	}
+	if p.Provider.Endpoint == "" {
+		return d.Err("missing Endpoint")
+	}
+	if p.Provider.Context == "" {
+		return d.Err("missing Context")
+	}
+	return nil
+}
+
+// Interface guards
+var (
+	_ caddyfile.Unmarshaler = (*Provider)(nil)
+	_ caddy.Provisioner     = (*Provider)(nil)
+)
